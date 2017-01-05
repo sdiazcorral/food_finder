f4e7692038518d3352907ea175c7f58c0f1d686f
Stanley <sdiazcorral@gmail.com>
2017-01-04 17:29:11 -0800

    Added filter capability

--- lib/guide.rb
+++ lib/guide.rb
@@ -47,10 +47,10 @@ class Guide
     result = nil # initialize variable so we can use it
     until result == :quit
       # make sure input is valid by running it through get_action method
-      action = get_action
+      action, args = get_action
       # do_action needs an argument
       # send user_response value to do_action as an argument
-      result = do_action(action)
+      result = do_action(action, args)
     end
     conclusion
   end
@@ -64,18 +64,20 @@ class Guide
       # save the input in user_response var
       user_response = gets.chomp
       # we want to downcase it and strip to make sure it works with our list of actions
-      action = user_response.downcase.strip
+      args = user_response.downcase.strip.split(" ")
+      action = args.shift
     end
-    return action
+    return action, args
   end
 
   # do_action accepts an argument called action
-  def do_action(action)
+  def do_action(action, args=[])
     case action
     when "list"
       list
     when "find"
-      puts "Finding..."
+      keyword = args.shift
+      find(keyword)
     when "add"
       add
     when "quit"
@@ -90,6 +92,22 @@ class Guide
     output_action_header("Listing restaurants")
     restaurants = Restaurant.saved_restaurants
     output_restaurant_table(restaurants)
+  end
+
+  def find(keyword="")
+    output_action_header("Find a restaurant")
+    if keyword
+      restaurants = Restaurant.saved_restaurants
+      found = restaurants.select do |rest|
+        rest.name.downcase.include?(keyword.downcase) ||
+        rest.cuisine.downcase.include?(keyword.downcase) ||
+        rest.price.to_i <= keyword.to_i
+      end
+      output_restaurant_table(found)
+    else
+      puts "Find using a key phrase to search the restaurant list."
+      puts "Examples: 'find tamale', 'find mexican', 'find mex'\n\n"
+    end
   end
 
   def add
